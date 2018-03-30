3fabbfe3fdf6332b163f80bc7436949d22795531
Sebastian <sebastian.callh@gmail.com>
2018-03-30 09:32:02 +0200

    Added .project files and default.nix

--- default.nix
+++ default.nix
@@ -2,9 +2,10 @@
 
 (import ./reflex-platform {}).project ({ pkgs, ... }: {
   packages = {
-    common = ./common;
-    backend = ./backend;
-    frontend = ./frontend;
+    common  = ./common;
+    server  = ./server;
+    client  = ./client;
+    scraper = ./scraper;
   };
 
   android.frontend = {
@@ -20,7 +21,22 @@
   };
 
   shells = {
-    ghc = ["common" "backend" "frontend"];
-    ghcjs = ["common" "frontend"];
+    ghc = ["common" "server" "client" "scraper"];
+    ghcjs = ["common" "client"];
+  };
+
+  overrides = self: super: {
+    servant-reflex = self.callCabal2nix "servant-reflex" (pkgs.fetchFromGitHub {
+      owner  = "sebastiancallh";
+      repo   = "servant-reflex";
+      rev    = "2ca67f5d92750821e864ec5c968db3ecc5da92e1";
+      sha256 = "012d7n565ariw1zkrr1lsncicssfqwjdxa12ylh49wd9qfy4h54a";
+    }) {};
+#    megaparsec = self.callCabal2nix "megaparsec" (pkgs.fetchFromGitHub {
+#      owner  = "mrkkrp";
+#      repo   = "megaparsec";
+#      rev    = "e1dac295766a206e61eef25b46e645595069ff35";
+#      sha256 = "1dxs678by1f5h0xqkllqbr99dfqqsb8n1s2crly4286pdpgqzilk";
+#    }) {};
   };
 })
