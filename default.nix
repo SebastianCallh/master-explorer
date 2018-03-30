e302e977b8b24f0fe482adfeec571ddfb7cc35cb
Sebastian <sebastian.callh@gmail.com>
2018-03-30 09:40:33 +0200

    Removed comments in default file

--- default.nix
+++ default.nix
@@ -32,11 +32,5 @@
       rev    = "2ca67f5d92750821e864ec5c968db3ecc5da92e1";
       sha256 = "012d7n565ariw1zkrr1lsncicssfqwjdxa12ylh49wd9qfy4h54a";
     }) {};
-#    megaparsec = self.callCabal2nix "megaparsec" (pkgs.fetchFromGitHub {
-#      owner  = "mrkkrp";
-#      repo   = "megaparsec";
-#      rev    = "e1dac295766a206e61eef25b46e645595069ff35";
-#      sha256 = "1dxs678by1f5h0xqkllqbr99dfqqsb8n1s2crly4286pdpgqzilk";
-#    }) {};
   };
 })
