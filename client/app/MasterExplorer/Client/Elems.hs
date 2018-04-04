7671b0dea063b0d03aa96ed0f26ec108d7c97fe7
Sebastian <sebastian.callh@gmail.com>
2018-04-04 23:09:21 +0200

    Added a custom clickableDynText used by program and course lists

--- client/app/MasterExplorer/Client/Elems.hs
+++ client/app/MasterExplorer/Client/Elems.hs
@@ -14,18 +14,32 @@ import           Reflex.Dom
 import           MasterExplorer.Common.Class.ListItem   (ListItem, listItemText)
 import           MasterExplorer.Common.Class.FilterItem (FilterItem, filterFields)
 
-customButton :: MonadWidget t m => Text -> m (Event t ())
+customButton :: forall t m.
+  MonadWidget t m
+  => Text
+  -> m (Event t ())
 customButton txt = divClass "custom-button" $ button txt
 
-okButton :: MonadWidget t m => m (Event t ())
+okButton :: forall t m.
+  MonadWidget t m
+  => m (Event t ())
 okButton = divClass "ok-button" $ button "Ok"
 
-cancelButton :: MonadWidget t m => m (Event t ())
+cancelButton :: forall t m.
+  MonadWidget t m
+  => m (Event t ())
 cancelButton = divClass "cancel-button" $ button "Cancel"
 
-cssClass :: Text
-cssClass = "course-list"
- 
+clickableDynText :: forall t m.
+  (DomBuilder t m,
+   PostBuild t m)
+  => Dynamic t Text
+  -> m (Event t ())
+clickableDynText textDyn = do
+  (e,_) <- elDynAttr' "a" (("class" =: "clickable") <$ textDyn) $
+    dynText textDyn
+  return $ domEvent Click e
+
 itemList :: forall t m a.
   (MonadWidget t m,
    DomBuilder t m,
@@ -34,7 +48,7 @@ itemList :: forall t m a.
   => Dynamic t [a]
   -> m (Event t a)
 itemList itemsDyn = do
-  eventsDyn <- elClass "ul" cssClass $
+  eventsDyn <- el "ul" $
     simpleList itemsDyn listItem
   return $ switchPromptlyDyn $ leftmost <$> eventsDyn
 
@@ -47,8 +61,7 @@ listItem :: forall t m a.
   -> m (Event t a)
 listItem itemDyn =
   el "li" $ do
-    dynText $ listItemText <$> itemDyn
-    ev <- _link_clicked <$> link "Select"
+    ev <- clickableDynText $ listItemText <$> itemDyn
     return $ tagPromptlyDyn itemDyn ev
     
 filterList :: forall t m a.
