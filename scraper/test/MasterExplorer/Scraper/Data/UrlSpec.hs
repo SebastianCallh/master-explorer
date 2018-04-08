b41790c9a71c695b40ad851daf432f67ee711377
Sebastian <sebastian.callh@gmail.com>
2018-04-08 16:28:14 +0200

    Added test for parsing empty urls

--- scraper/test/MasterExplorer/Scraper/Data/UrlSpec.hs
+++ scraper/test/MasterExplorer/Scraper/Data/UrlSpec.hs
@@ -10,5 +10,7 @@ spec =
   describe "parseUrls" $ do
     it "parses an url" $ do
       input <- pack <$> readFile "markup/url.html"
-      parseUrls input `shouldBe` (Right $ [Url "http://www.isy.liu.se/en/edu/kurs/TSTE12/"])
+      parseUrls input `shouldBe` Right [Url "http://www.isy.liu.se/en/edu/kurs/TSTE12/"]
 
+    it "returns empty list on no urls" $
+      parseUrls "" `shouldBe` Right []
