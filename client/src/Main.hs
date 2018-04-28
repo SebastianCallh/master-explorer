b9def0dda433244308b7c7763a5c32af9af5ef6a
Sebastian <sebastian.callh@gmail.com>
2018-04-28 14:17:59 +0200

    Moved app out from Main and added prod/dev mode to be read from config file

--- client/src/Main.hs
+++ client/src/Main.hs
@@ -1,57 +1,30 @@
-{-# LANGUAGE DataKinds           #-}
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE RankNTypes          #-}
-{-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
-import           Language.Javascript.JSaddle.Warp
-import           Reflex.Dom                      hiding (mainWidgetWithCss, run)
-import           Reflex.Dom.Core                        (mainWidgetWithCss)
-import           Servant.Reflex
-import           Data.FileEmbed                         (embedFile)
+import qualified Reflex.Dom                       as Dom
+import qualified Reflex.Dom.Core                  as Core
 
-import           MasterExplorer.Common.Data.Program     (engPrograms)
-import           MasterExplorer.Client.ProgramList      (programList)
-import           MasterExplorer.Client.CourseGrid       (courseGrid)
-import           MasterExplorer.Client.Api              (programCourses)
-import           MasterExplorer.Client.CourseList       (courseList)
+import           Data.FileEmbed                   (embedFile)
+import           Language.Javascript.JSaddle.Warp
+import           Servant.Reflex
+
+import           MasterExplorer.Client.App        (app)
+
+data Mode
+  = Development
+  | Production
 
 main :: IO ()
-main = let css = $(embedFile "css/style.css") in
-         run 3911 $ mainWidgetWithCss css app
---main = mainWidget body
---let url = constDyn $ BasePath "http://localhost:8080"
--- neat font <link href="https://fonts.googleapis.com/css?family=Abel" rel="stylesheet">
+main = runApp Development
 
-app :: forall t m.
-  (MonadWidget t m,
-   DomBuilder t m)  
-  => m ()
-app =
-  divClass "container" $ do
-    programSelectEv <- divClass "header" $
-      programList $ constDyn engPrograms
-  
-    let url = constDyn $ BasePath "http://localhost:8080"
-    coursesEv  <- programCourses url programSelectEv
-    coursesDyn <- holdDyn [] coursesEv
-  
-    courseSelectEv <- divClass "sidebar" $
-      courseList coursesDyn
+runApp :: Mode -> IO ()
+runApp Development =
+  run 3911 $ Core.mainWidgetWithCss css $ app apiUrl
+  where css    = $(embedFile "css/style.css")
+        apiUrl = Dom.constDyn $ BasePath "http://localhost:8080"
 
-    divClass "info-bar" $
-      text "HP: 12"
-  
-    _courseClicks <- divClass "content" $
-      courseGrid courseSelectEv
-
-    
-    divClass "footer" $ pure ()
-  
-    pure ()
-    
-
+runApp Production =
+  Dom.mainWidgetWithCss css $ app apiUrl
+  where css    = $(embedFile "css/style.css")
+        apiUrl = Dom.constDyn $ BasePath "http://localhost:8080"
---
+++ client/src/MasterExplorer/Client/App.hs
@@ -0,0 +1,40 @@
+module MasterExplorer.Client.App
+  where
+
+import           Reflex.Dom.Extended
+import           Servant.Reflex                         (BaseUrl)
+
+import           MasterExplorer.Common.Data.Program     (engPrograms)
+import           MasterExplorer.Client.ProgramList      (programList)
+import           MasterExplorer.Client.CourseGrid       (courseGrid)
+import           MasterExplorer.Client.Api              (programCourses)
+import           MasterExplorer.Client.CourseList       (courseList)
+
+app :: forall t m.
+  (MonadWidget t m,
+   DomBuilder t m)  
+  => Dynamic t BaseUrl 
+  -> m ()
+app apiUrlDyn =
+  divClass "container" $ do
+    programSelectEv <- divClass "header" $
+      programList $ constDyn engPrograms
+  
+    coursesEv  <- programCourses apiUrlDyn programSelectEv
+    coursesDyn <- holdDyn [] coursesEv
+  
+    courseSelectEv <- divClass "sidebar" $
+      courseList coursesDyn
+
+    divClass "info-bar" $
+      text "HP: 12"
+  
+    _courseClicks <- divClass "content" $
+      courseGrid courseSelectEv
+
+    
+    divClass "footer" $ pure ()
+  
+    pure ()
+    
+
