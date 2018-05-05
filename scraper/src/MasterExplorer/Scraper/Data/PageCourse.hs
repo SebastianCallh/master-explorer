1ff66815b19a6bc91553e79644cab57fa55b3d58
Sebastian <sebastian.callh@gmail.com>
2018-05-05 09:16:01 +0200

    Moved fromSection function (parsing PageCourse) into Scrapers

--- scraper/src/MasterExplorer/Scraper/Data/PageCourse.hs
+++ scraper/src/MasterExplorer/Scraper/Data/PageCourse.hs
@@ -1,35 +1,22 @@
 module MasterExplorer.Scraper.Data.PageCourse
   ( PageCourse(..)
-  , fromPageSections
   ) where
 
 -- Course info that was parsed from a course page
 
-import qualified Data.Map                                  as M
 
-import           Data.Map                                  (Map)
-import           Data.Maybe                                (fromMaybe)
-import           Data.Text                                 (Text)
-
-import           MasterExplorer.Scraper.Data.Area          (Area, parseAreas)
-import           MasterExplorer.Scraper.Data.CourseContent (CourseContent,
-                                                            parseContent)
-import           MasterExplorer.Scraper.Data.Examination   (Examination,
-                                                            parseExaminations)
+import           MasterExplorer.Scraper.Data.Area          (Area)
+import           MasterExplorer.Scraper.Data.CourseContent (CourseContent)
+import           MasterExplorer.Scraper.Data.Examination   (Examination)
 import           MasterExplorer.Scraper.Data.Examinator    (Examinator (..))
-import           MasterExplorer.Scraper.Data.Field         (Field, parseField)
-import           MasterExplorer.Scraper.Data.Grading       (Grading,
-                                                            parseGrading)
-import           MasterExplorer.Scraper.Data.Hours         (Hours, parseHours)
-import           MasterExplorer.Scraper.Data.Institution   (Institution,
-                                                            parseInstitution)
+import           MasterExplorer.Scraper.Data.Field         (Field)
+import           MasterExplorer.Scraper.Data.Grading       (Grading)
+import           MasterExplorer.Scraper.Data.Hours         (Hours)
+import           MasterExplorer.Scraper.Data.Institution   (Institution)
 import           MasterExplorer.Scraper.Data.Prerequisites (Prerequisites (..))
-import           MasterExplorer.Scraper.Data.Program       (Program,
-                                                            parsePrograms)
-import           MasterExplorer.Scraper.Data.Subject       (Subject,
-                                                            parseSubjects)
-import           MasterExplorer.Scraper.Data.Url           (Url (..), parseUrls)
-import           MasterExplorer.Scraper.Helpers            (eitherToMaybe)
+import           MasterExplorer.Scraper.Data.Program       (Program)
+import           MasterExplorer.Scraper.Data.Subject       (Subject)
+import           MasterExplorer.Scraper.Data.Url           (Url (..))
 
 data PageCourse = PageCourse
   { pCourseAreas         :: ![Area]
@@ -46,28 +33,3 @@ data PageCourse = PageCourse
   , pCourseSelfStudyTime :: !(Maybe Hours)
   , pCourseScheduledTime :: !(Maybe Hours)
   } deriving (Show)
-
-fromPageSections :: Map Text Text -> PageCourse
-fromPageSections sections = PageCourse
-  { pCourseAreas         = parseList parseAreas        "Huvudomr\229de"
-  , pCourseInstitution   = parse parseInstitution      "Institution"
-  , pCoursePrograms      = maybe [] parsePrograms $ M.lookup  "Kursen ges f\246r" sections
-  , pCourseField         = parse parseField            "Utbildningsomr\229de"
-  , pCoursePrerequisites = Prerequisites <$> M.lookup  "F\246rkunskapskrav" sections
-  , pCourseGrading       = parse parseGrading          "Betygsskala"
-  , pCourseExaminator    = Examinator <$> M.lookup     "Examinator" sections
-  , pCourseExaminations  = parseList parseExaminations "Examination"
-  , pCourseContent       = parse parseContent          "Kursinneh\229ll"
-  , pCourseSubject       = parseList parseSubjects     "\196mnesomr\229de"
-  , pCourseUrls          = parseList parseUrls         "Kurshemsida och andra l\228nkar"
-  , pCourseScheduledTime = scheduledTime
-  , pCourseSelfStudyTime = selfStudyTime
-  }
-  where
-    parse :: (Text -> Either Text a) -> Text -> Maybe a
-    parse parser key = maybe Nothing (eitherToMaybe . parser) $ M.lookup key sections
-    parseList parser key = fromMaybe [] $ parse parser key
-    (scheduledTime, selfStudyTime) =
-      case parse parseHours "Undervisningstid" of
-        Nothing     -> (Nothing, Nothing)
-        Just (a, b) -> (Just a, Just b)
--- scraper/src/MasterExplorer/Scraper/Web/Scrapers.hs
+++ scraper/src/MasterExplorer/Scraper/Web/Scrapers.hs
@@ -9,8 +9,11 @@ import qualified Data.Map.Strict                            as M
 import qualified Data.Text                                  as T
 
 import           Data.Either                                (partitionEithers)
+import           Data.Map                                   (Map)
+import           Data.Maybe                                 (fromMaybe)
 import           Data.Semigroup                             ((<>))
 import           Data.Text                                  (Text)
+import           MasterExplorer.Scraper.Helpers             (eitherToMaybe)
 import           Text.HTML.Scalpel                          (Scraper, attr,
                                                              chroot, chroots,
                                                              hasClass,
@@ -19,39 +22,82 @@ import           Text.HTML.Scalpel                          (Scraper, attr,
                                                              scrapeStringLike,
                                                              text, (@:))
 
+import           MasterExplorer.Scraper.Data.Area           (Area, parseAreas)
 import           MasterExplorer.Scraper.Data.Block          (Block, parseBlocks)
 import           MasterExplorer.Scraper.Data.CourseCode     (CourseCode (..))
+import           MasterExplorer.Scraper.Data.CourseContent  (CourseContent,
+                                                             parseContent)
 import           MasterExplorer.Scraper.Data.CourseName     (CourseName (..))
 import           MasterExplorer.Scraper.Data.Credits        (Credits,
                                                              parseCredits)
+import           MasterExplorer.Scraper.Data.Examination    (Examination,
+                                                             parseExaminations)
+import           MasterExplorer.Scraper.Data.Examinator     (Examinator (..))
+import           MasterExplorer.Scraper.Data.Field          (Field, parseField)
+import           MasterExplorer.Scraper.Data.Grading        (Grading,
+                                                             parseGrading)
+import           MasterExplorer.Scraper.Data.Hours          (Hours, parseHours)
 import           MasterExplorer.Scraper.Data.Importance     (Importance,
                                                              parseImportance)
+import           MasterExplorer.Scraper.Data.Institution    (Institution,
+                                                             parseInstitution)
 import           MasterExplorer.Scraper.Data.Level          (Level, parseLevel)
 import           MasterExplorer.Scraper.Data.ListCourse     (ListCourse (..))
 import           MasterExplorer.Scraper.Data.Occasion       (Occasion (..))
-import           MasterExplorer.Scraper.Data.PageCourse     (PageCourse (..),
-                                                             fromPageSections)
+import           MasterExplorer.Scraper.Data.PageCourse     (PageCourse (..))
 import           MasterExplorer.Scraper.Data.Period         (Period,
                                                              parsePeriod)
+import           MasterExplorer.Scraper.Data.Prerequisites  (Prerequisites (..))
 import           MasterExplorer.Scraper.Data.Program        (Program)
+import           MasterExplorer.Scraper.Data.Program        (Program,
+                                                             parsePrograms)
 import           MasterExplorer.Scraper.Data.Semester       (Semester,
                                                              parseSemester)
 import           MasterExplorer.Scraper.Data.Slot           (Slot (..))
 import           MasterExplorer.Scraper.Data.Specialization (Specialization,
                                                              parseSpecialization)
+import           MasterExplorer.Scraper.Data.Subject        (Subject,
+                                                             parseSubjects)
 import           MasterExplorer.Scraper.Data.Url            (Url (..))
+import           MasterExplorer.Scraper.Data.Url            (Url (..),
+                                                             parseUrls)
 import           MasterExplorer.Scraper.Helpers             (maybeToEither)
 
 type Scalpel = Scraper Text
 
 pageCourseScraper :: Scalpel PageCourse
 pageCourseScraper =
-  fmap (fromPageSections . M.fromList) $
+  fmap (makePageCourse . M.fromList) $
   chroot ("section" @: [hasClass "studyguide-block"]) $
     chroots "div" $ do
       title   <- innerHTML "h3"
       content <- sanitize . snd . T.breakOnEnd "</h3>" <$> innerHTML "div"
       return (title, content)
+
+makePageCourse :: Map Text Text -> PageCourse
+makePageCourse sections = PageCourse
+  { pCourseAreas         = parseList parseAreas        "Huvudomr\229de"
+  , pCourseInstitution   = parse parseInstitution      "Institution"
+  , pCoursePrograms      = maybe [] parsePrograms $ M.lookup  "Kursen ges f\246r" sections
+  , pCourseField         = parse parseField            "Utbildningsomr\229de"
+  , pCoursePrerequisites = Prerequisites <$> M.lookup  "F\246rkunskapskrav" sections
+  , pCourseGrading       = parse parseGrading          "Betygsskala"
+  , pCourseExaminator    = Examinator <$> M.lookup     "Examinator" sections
+  , pCourseExaminations  = parseList parseExaminations "Examination"
+  , pCourseContent       = parse parseContent          "Kursinneh\229ll"
+  , pCourseSubject       = parseList parseSubjects     "\196mnesomr\229de"
+  , pCourseUrls          = parseList parseUrls         "Kurshemsida och andra l\228nkar"
+  , pCourseScheduledTime = scheduledTime
+  , pCourseSelfStudyTime = selfStudyTime
+  }
+  where
+    parse :: (Text -> Either Text a) -> Text -> Maybe a
+    parse parser key = maybe Nothing (eitherToMaybe . parser) $ M.lookup key sections
+    parseList parser key = fromMaybe [] $ parse parser key
+    (scheduledTime, selfStudyTime) =
+      case parse parseHours "Undervisningstid" of
+        Nothing     -> (Nothing, Nothing)
+        Just (a, b) -> (Just a, Just b)
 
 programplanScraper :: Program -> Scalpel [Either Text ListCourse]
 programplanScraper program =
