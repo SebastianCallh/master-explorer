3b941c8643b07313897e5e904a71a1dd3e5e1458
Sebastian <sebastian.callh@gmail.com>
2018-05-06 19:06:14 +0200

    Pretty instance for subject

--- common/src/MasterExplorer/Common/Data/Subject.hs
+++ common/src/MasterExplorer/Common/Data/Subject.hs
@@ -5,31 +5,56 @@ module MasterExplorer.Common.Data.Subject
   ( Subject (..)
   ) where
 
-import           Data.Aeson   (FromJSON, ToJSON)
-import           GHC.Generics (Generic)
+import           Data.Aeson                         (FromJSON, ToJSON)
+import           GHC.Generics                       (Generic)
+
+import           MasterExplorer.Common.Class.Pretty (Pretty, pretty)
 
 data Subject
   = ComputerScience
-  | Electrotechnics
   | Electronics
-  | EnvironmentProtection
+  | Electrotechnics
   | English
+  | EnvironmentProtection
   | French
   | German
   | History
   | Informatics
   | Law
-  | LeadershipOrganisation
   | Leadership
+  | LeadershipOrganisation
   | Matematik
   | MediaCommunication
   | MediaProduction
   | Organisation
+  | Other
   | OtherMedicine
   | OtherTechnical
-  | Physics
   | Philosophy
+  | Physics
   | Spanish
-  | Other
   deriving (Show, Read, Generic, ToJSON, FromJSON)
 
+instance Pretty Subject where
+  pretty ComputerScience        = "Datateknik"
+  pretty Electronics            = "Elektronik"
+  pretty Electrotechnics        = "Elektroteknik"
+  pretty English                = "Engelska"
+  pretty EnvironmentProtection  = "Milj\246v\229rd och milj\246skydd"
+  pretty French                 = "Franska"
+  pretty German                 = "Tyska"
+  pretty History                = "Historia"
+  pretty Informatics            = "Informatik/Data- och systemvetenskap"
+  pretty Law                    ="Juridik och rättsvetenskap"
+  pretty Leadership             = "Ledarskap"
+  pretty LeadershipOrganisation = "Ledarskap, organisation och styrning"
+  pretty Matematik              = "Matematik"
+  pretty MediaCommunication     = "Medie- o kommunikationsvetenskap"
+  pretty MediaProduction        = "Medieproduktion"
+  pretty Organisation           = "Industriell ekonomi och organisation"
+  pretty Other                  = "Övriga ämnen"
+  pretty OtherMedicine          = "\214vrigt inom medicin"
+  pretty OtherTechnical         = "\214vriga tekniska \228mnen"
+  pretty Philosophy             = "Filosofi"
+  pretty Physics                = "Fysik"
+  pretty Spanish                = "Spanska"
