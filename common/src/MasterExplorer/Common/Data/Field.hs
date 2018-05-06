bf800842bbf070fc8895eb2e28825b54f240f2b8
Sebastian <sebastian.callh@gmail.com>
2018-05-06 19:06:05 +0200

    Pretty instance for field

--- common/src/MasterExplorer/Common/Data/Field.hs
+++ common/src/MasterExplorer/Common/Data/Field.hs
@@ -6,14 +6,24 @@ module MasterExplorer.Common.Data.Field
   ) where
 
 
-import           Data.Aeson   (FromJSON, ToJSON)
-import           GHC.Generics (Generic)
+import           Data.Aeson                         (FromJSON, ToJSON)
+import           GHC.Generics                       (Generic)
+
+import           MasterExplorer.Common.Class.Pretty (Pretty, pretty)
 
 data Field
   = Humanities
+  | Law
   | Medicine
-  | Technical
   | Science
   | Society
-  | Law
+  | Technical
   deriving (Show, Read, Eq, Generic, ToJSON, FromJSON)
+
+instance Pretty Field where
+  pretty Humanities = "Humanistiska"
+  pretty Law        = "Medicinska"
+  pretty Medicine   = "Tekniska"
+  pretty Science    = "Naturvetenskapliga"
+  pretty Society    = "Samhällsvetenskapliga"
+  pretty Technical  = "Juridiska"
