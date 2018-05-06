225cdd09c89aecee8cd35e7fae6a4a82ef511e23
Sebastian <sebastian.callh@gmail.com>
2018-05-06 19:05:43 +0200

    Pretty instance for examinator

--- common/src/MasterExplorer/Common/Data/Examinator.hs
+++ common/src/MasterExplorer/Common/Data/Examinator.hs
@@ -5,9 +5,14 @@ module MasterExplorer.Common.Data.Examinator
   ( Examinator (..)
   ) where
 
-import           Data.Aeson   (FromJSON, ToJSON)
-import           Data.Text    (Text)
-import           GHC.Generics (Generic)
+import           Data.Aeson                         (FromJSON, ToJSON)
+import           Data.Text                          (Text)
+import           GHC.Generics                       (Generic)
+
+import           MasterExplorer.Common.Class.Pretty (Pretty, pretty)
 
 newtype Examinator = Examinator { getExaminator :: Text }
     deriving (Show, Read, Generic, ToJSON, FromJSON)
+
+instance Pretty Examinator where
+  pretty = getExaminator
