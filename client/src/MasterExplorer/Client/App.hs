485481089887a5a2c7718130c5d34abe8e5960d7
Sebastian <sebastian.callh@gmail.com>
2018-05-15 16:22:26 +0200

    Organized App.hs and fixed bug where loading a scheduled caused the app to fall out of synch.

--- client/src/MasterExplorer/Client/App.hs
+++ client/src/MasterExplorer/Client/App.hs
@@ -10,8 +10,9 @@ import qualified Data.Map   as M
 import           Control.Lens
 import           Servant.Reflex                         (BaseUrl)
 import           Reflex.Dom.Extended
-import           MasterExplorer.Common.Data.Program     (engPrograms)
-import           MasterExplorer.Common.Data.Schedule   
+import           MasterExplorer.Common.Data.Program     (Program, engPrograms)
+import           MasterExplorer.Common.Data.Course      (Course)
+import           MasterExplorer.Common.Data.Schedule    (Schedule (..))
 
 import qualified MasterExplorer.Client.Api               as API
 import qualified MasterExplorer.Client.EmptyContent      as EC
@@ -19,6 +20,7 @@ import qualified MasterExplorer.Client.Content           as C
 import qualified MasterExplorer.Client.CourseList        as CL
 import qualified MasterExplorer.Client.ProgramList       as PL
 import qualified MasterExplorer.Client.ScheduleApiMenu   as SAM
+import qualified MasterExplorer.Common.Data.Schedule     as Schedule
 
 app :: forall t m.
   MonadWidget t m
@@ -27,36 +29,64 @@ app :: forall t m.
 app apiUrlDyn = do
   api <- API.widget apiUrlDyn
   divClass "container" $ do
-    programList <- divClass "header" $ do
-      divClass "logo" $ text "Master Explorer"
-      let getCourses  = api ^. API.getProgramCourses
-      let programsDyn = constDyn engPrograms
-      PL.widget getCourses programsDyn
-
-    courseList <- divClass "sidebar" $
-      CL.widget (programList ^. PL.selectedCourses)
-        
-    _ <- divClass "content" $ do
-      rec 
-        content <- dyn $ ffor (programList ^. PL.selectedProgram) $ \case
-          Nothing -> EC.widget
-          Just _  -> C.widget
-                     scheduleDyn
-                     (courseList ^. CL.focusedCourse)
-                     
-          
-        scheduleApiMenu <- SAM.widget
-          (api ^. API.saveSchedule)
-          (api ^. API.loadSchedule)
-          scheduleDyn
-        
-        scheduleDyn <- foldDynMaybe const (Schedule M.empty) $ leftmost
-          [ Just . Schedule <$> updated (courseList ^. CL.slots)
-          , scheduleApiMenu ^. SAM.onLoad
-          ]
-          
-      return content
+    rec
+      programList <- header api      
+      courseList  <- sidebar programList        
+      scheduleDyn <- content programList courseList api          
+      return ()
           
     divClass "footer" $ pure ()
   
     pure ()
+    
+header :: forall t m.
+  MonadWidget t m
+  => API.Api t m  
+  -> m (PL.ProgramList t)
+header api =
+  divClass "header" $ do
+    divClass "logo" $ text "Master Explorer"
+    let getCourses  = api ^. API.getProgramCourses
+    let programsDyn = constDyn engPrograms
+    PL.widget getCourses programsDyn
+
+sidebar :: forall t m.
+  MonadWidget t m
+  => PL.ProgramList t
+  -> m (CL.CourseList t)
+sidebar programList =
+  divClass "sidebar" $ 
+    CL.widget  $ programList ^. PL.selectedCourses
+
+content :: forall t m.
+  MonadWidget t m
+  => PL.ProgramList t
+  -> CL.CourseList t
+  -> API.Api t m
+  -> m (Dynamic t Schedule)
+content programList courseList api  =
+  divClass "content" $ mdo --
+    _ <- dyn $ ffor (programList ^. PL.selectedProgram) $ \case
+      Nothing -> EC.widget
+      Just _  -> C.widget
+                 scheduleDyn
+                 (courseList ^. CL.focusedCourse)
+                     
+    scheduleApiMenu <- SAM.widget
+      (api ^. API.saveSchedule)
+      (api ^. API.loadSchedule)
+      scheduleDyn
+
+    let setScheduleEv = const <$> scheduleApiMenu ^. SAM.onLoad
+
+    -- Need to inject the output of the function into Maybe
+    let addCourseToScheduleEv = fmap pure .
+          uncurry Schedule.addSelection <$>
+          (courseList ^. CL.onCourseSelect)
+          
+    scheduleDyn <- foldDynMaybe ($) (Schedule M.empty) $ leftmost
+        [ addCourseToScheduleEv
+        , setScheduleEv
+        ]
+  
+    return scheduleDyn
--- common/src/MasterExplorer/Common/Data/Schedule.hs
+++ common/src/MasterExplorer/Common/Data/Schedule.hs
@@ -1,23 +1,24 @@
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DeriveGeneric  #-}
 
-module MasterExplorer.Common.Data.Schedule
-  ( Schedule (..)
-  ) where
+module MasterExplorer.Common.Data.Schedule where
 
-import qualified Data.Text                         as T
+import qualified Data.List                           as L
+import qualified Data.Map                            as M
+import qualified Data.Text                           as T
 
-import           Data.Aeson                        (FromJSON, ToJSON)
-import           Data.Bifunctor                    (bimap)
-import           Data.Map                          (Map)
-import           GHC.Generics                      (Generic)
-import           Servant.API                       (FromHttpApiData,
-                                                    ToHttpApiData,
-                                                    parseUrlPiece, toUrlPiece)
-import           Text.Read                         (readEither)
+import           Data.Aeson                          (FromJSON, ToJSON)
+import           Data.Bifunctor                      (bimap)
+import           Data.Map                            (Map)
+import           GHC.Generics                        (Generic)
+import           Servant.API                         (FromHttpApiData,
+                                                      ToHttpApiData,
+                                                      parseUrlPiece, toUrlPiece)
+import           Text.Read                           (readEither)
 
-import           MasterExplorer.Common.Data.Course (Course)
-import           MasterExplorer.Common.Data.Slot   (Slot)
+import           MasterExplorer.Common.Data.Course   (Course)
+import           MasterExplorer.Common.Data.Occasion (Occasion (..))
+import           MasterExplorer.Common.Data.Slot     (Slot)
 
 -- | A schedule with a set of selected courses.
 newtype Schedule = Schedule { getSchedule :: Map Slot [Course] }
@@ -29,3 +30,15 @@ instance FromHttpApiData Schedule where
 
 instance ToHttpApiData Schedule where
   toUrlPiece = T.pack . show
+
+addSelection :: Course -> Occasion -> Schedule -> Schedule
+addSelection course occasion schedule =
+  Schedule $ foldr (addToSlots course) (getSchedule schedule) (getOccasion occasion)
+  where
+    addToSlots c s = M.insertWith (++) s [c]
+
+removeSelection :: Course -> Occasion -> Schedule -> Schedule
+removeSelection course occasion schedule =
+  Schedule $ foldr (removeFromSlots course) (getSchedule schedule) (getOccasion occasion)
+  where
+    removeFromSlots c = M.adjust (L.delete c)
